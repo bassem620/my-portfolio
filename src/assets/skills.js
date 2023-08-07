56a67fea4b355816e3411865cd12d5908e550a9e
Bassem S.Mosaad <bassemsaeed620@gmail.com>
2023-08-07 23:50:00 +0300

    Skills v1.0 & Logos

---
+++ src/assets/skills.js
@@ -0,0 +1,90 @@
+// import javascript from '../assets/logos/javascript.svg'
+// import typescript from '../assets/logos/typescript.svg'
+// import node from '../assets/logos/node.svg'
+// import react from '../assets/logos/react.svg'
+// import mongo from '../assets/logos/mongo.svg'
+// import python from '../assets/logos/python.svg'
+// import Docker from '../assets/logos/Docker.svg'
+// import sass from '../assets/logos/sass.svg'
+// import git from '../assets/logos/git.svg'
+// import figma from '../assets/logos/figma.svg'
+
+import javascript from '../components/logos/JavascriptLogo.jsx'
+import typescript from '../components/logos/TypeScriptLogo.jsx'
+import node from '../components/logos/NodeLogo.jsx'
+import react from '../components/logos/ReactLogo.jsx'
+import mongo from '../components/logos/MongoLogo.jsx'
+import python from '../components/logos/PythonLogo.jsx'
+import Docker from '../components/logos/DockerLogo.jsx'
+import sass from '../components/logos/SassLogo.jsx'
+import git from '../components/logos/GitLogo.jsx'
+import figma from '../components/logos/FigmaLogo.jsx'
+
+const skills = [
+    {
+        name: 'Javascript',
+        logo: javascript
+    },
+    {
+        name: 'Typescript',
+        logo: typescript
+    },
+    {
+        name: 'NodeJS',
+        logo: node
+    },
+    {
+        name: 'ExpressJS',
+        logo: undefined
+    },
+    {
+        name: 'ReactJS',
+        logo: react
+    },
+    {
+        name: 'MongoDB',
+        logo: mongo
+    },
+    {
+        name: 'SQL',
+        logo: undefined
+    },
+    {
+        name: 'Python',
+        logo: python
+    },
+    {
+        name: 'Docker',
+        logo: Docker
+    },
+    {
+        name: 'SASS',
+        logo: sass
+    },
+    {
+        name: 'Git',
+        logo: git
+    },
+    {
+        name: 'RESTful API',
+        logo: null
+    },
+    {
+        name: 'Agile',
+        logo: null
+    },
+    {
+        name: 'Linux',
+        logo: null
+    },
+    {
+        name: 'Figma',
+        logo: figma
+    },
+    {
+        name: 'Postman',
+        logo: undefined
+    },
+]
+
+export default skills;
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,7 +1,25 @@
+import { Container } from "react-bootstrap"
+
+import skills from '../assets/skills';
+
 const Skills = () => {
     return (
-        <div className="section" id="skills">
-            Skills
+        <div className="skills section" id="skills">
+            <Container>
+                <div className="textBox mx-auto mb-5">
+                    <span>🧑‍💻 Technologies and skills</span>
+                </div>
+                <div className="skillsContainer">
+                    {
+                        skills.map((sk, ind) => (
+                            <div className="skill" key={ind}>
+                                { sk.logo && <sk.logo color='#ffffff'/>}
+                                <span>{sk.name}</span>
+                            </div>
+                        ))
+                    }
+                </div>
+            </Container>
         </div>
     )
 }
