5cb277d5a7c77cb53116b1f46f7d082937f0e686
Bassem S.Mosaad <bassemsaeed620@gmail.com>
2023-08-07 19:43:01 +0300

    NavLinks active change on scroll - White var added

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,30 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
 
 const Nav = () => {
     const [mobileMenuActive, setMobileMenuActive] = useState(false);
-    const [activeLink, setActiveLink] = useState('Home');
+    const [activeLink, setActiveLink] = useState('home');
 
     const toggleMobileMenu = _ => setMobileMenuActive(!mobileMenuActive);
     const handleNavLinkClick = (link) => setActiveLink(link);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const sections = document.querySelectorAll('.section');
+            sections.forEach((section) => {
+                const sectionTop = section.offsetTop;
+                const sectionHeight = section.clientHeight;
+                if (window.scrollY >= sectionTop - sectionHeight * 0.5 && window.scrollY < sectionTop + sectionHeight * 0.5) {
+                    setActiveLink(section.getAttribute('id'));
+                }
+            });
+        };
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
 
     return (
     <div className={`nav ${mobileMenuActive ? 'active' : ''}`}>
@@ -15,42 +32,48 @@ const Nav = () => {
             <div className="navBox">
                 <div className="navTitle">BASSEM</div>
                 <div className="navLinks">
-                    <div
-                    className={`navLink ${activeLink === 'Home' ? 'active' : ''}`}
-                    onClick={ _ => handleNavLinkClick('Home')}
+                    <a
+                    href='#home'
+                    className={`navLink ${activeLink === 'home' ? 'active' : ''}`}
+                    onClick={ _ => handleNavLinkClick('home')}
                     >
                     Home
-                    </div>
-                    <div
-                    className={`navLink ${activeLink === 'About' ? 'active' : ''}`}
-                    onClick={ _ => handleNavLinkClick('About')}
+                    </a>
+                    <a
+                    href='#about'
+                    className={`navLink ${activeLink === 'about' ? 'active' : ''}`}
+                    onClick={ _ => handleNavLinkClick('about')}
                     >
                     About
-                    </div>
-                    <div
-                    className={`navLink ${activeLink === 'Skills' ? 'active' : ''}`}
-                    onClick={ _ => handleNavLinkClick('Skills')}
+                    </a>
+                    <a
+                    href='#skills'
+                    className={`navLink ${activeLink === 'skills' ? 'active' : ''}`}
+                    onClick={ _ => handleNavLinkClick('skills')}
                     >
                     Skills
-                    </div>
-                    <div
-                    className={`navLink ${activeLink === 'Projects' ? 'active' : ''}`}
-                    onClick={ _ => handleNavLinkClick('Projects')}
+                    </a>
+                    <a
+                    href='#projects'
+                    className={`navLink ${activeLink === 'projects' ? 'active' : ''}`}
+                    onClick={ _ => handleNavLinkClick('projects')}
                     >
                     Projects
-                    </div>
-                    <div
-                    className={`navLink ${activeLink === 'Experience' ? 'active' : ''}`}
-                    onClick={ _ => handleNavLinkClick('Experience')}
+                    </a>
+                    <a
+                    href='#experiences'
+                    className={`navLink ${activeLink === 'experience' ? 'active' : ''}`}
+                    onClick={ _ => handleNavLinkClick('experience')}
                     >
                     Experience
-                    </div>
-                    <div
-                    className={`navLink ${activeLink === 'Contact' ? 'active' : ''}`}
-                    onClick={ _ => handleNavLinkClick('Contact')}
+                    </a>
+                    <a
+                    href='#contact'
+                    className={`navLink ${activeLink === 'contact' ? 'active' : ''}`}
+                    onClick={ _ => handleNavLinkClick('contact')}
                     >
                     Contact
-                    </div>
+                    </a>
                 </div>
                 <div className="mobileMenuToggle d-none" onClick={toggleMobileMenu}>
                     <MenuRoundedIcon />
