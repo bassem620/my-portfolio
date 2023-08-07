3af2840aaea0e94cfd11154f2aa92f5d319b410a
Bassem S.Mosaad <bassemsaeed620@gmail.com>
2023-08-07 06:24:04 +0300

    Navbar v1.0

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,64 @@
-const Nav = () => {
-    return (
-        <div>Nav</div>
-    )
-}
+import React, { useState } from 'react'
 
-export default Nav
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+
+const Nav = () => {
+    const [mobileMenuActive, setMobileMenuActive] = useState(false);
+    const [activeLink, setActiveLink] = useState('Home');
+
+    const toggleMobileMenu = _ => setMobileMenuActive(!mobileMenuActive);
+    const handleNavLinkClick = (link) => setActiveLink(link);
+
+    return (
+    <div className={`nav ${mobileMenuActive ? 'active' : ''}`}>
+        <div className="container">
+            <div className="navBox">
+                <div className="navTitle">BASSEM</div>
+                <div className="navLinks">
+                    <div
+                    className={`navLink ${activeLink === 'Home' ? 'active' : ''}`}
+                    onClick={ _ => handleNavLinkClick('Home')}
+                    >
+                    Home
+                    </div>
+                    <div
+                    className={`navLink ${activeLink === 'About' ? 'active' : ''}`}
+                    onClick={ _ => handleNavLinkClick('About')}
+                    >
+                    About
+                    </div>
+                    <div
+                    className={`navLink ${activeLink === 'Skills' ? 'active' : ''}`}
+                    onClick={ _ => handleNavLinkClick('Skills')}
+                    >
+                    Skills
+                    </div>
+                    <div
+                    className={`navLink ${activeLink === 'Projects' ? 'active' : ''}`}
+                    onClick={ _ => handleNavLinkClick('Projects')}
+                    >
+                    Projects
+                    </div>
+                    <div
+                    className={`navLink ${activeLink === 'Experience' ? 'active' : ''}`}
+                    onClick={ _ => handleNavLinkClick('Experience')}
+                    >
+                    Experience
+                    </div>
+                    <div
+                    className={`navLink ${activeLink === 'Contact' ? 'active' : ''}`}
+                    onClick={ _ => handleNavLinkClick('Contact')}
+                    >
+                    Contact
+                    </div>
+                </div>
+                <div className="mobileMenuToggle d-none" onClick={toggleMobileMenu}>
+                    <MenuRoundedIcon />
+                </div>
+            </div>
+        </div>
+    </div>
+    );
+};
+
+export default Nav;
