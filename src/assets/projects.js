41a2f2a48917b193ae462a5c0bd617d4deb6f5c1
Bassem S.Mosaad <bassemsaeed620@gmail.com>
2023-08-11 10:52:55 +0300

    projects array init

---
+++ src/assets/projects.js
@@ -0,0 +1,25 @@
+const projects = [
+    {
+        id: 0,
+        title: "title",
+        desc: "description",
+        spec: "spec",
+        images: "",
+        links:[
+            {
+                name: 'Github',
+                link: 'https://github.com/',
+            },
+            {
+                name: 'Host',
+                link: 'https://render.com/',
+            },
+            {
+                name: 'LinkedIn Post',
+                link: 'https://linked-in.com/',
+            }
+        ],
+    },
+];
+
+export default projects;
