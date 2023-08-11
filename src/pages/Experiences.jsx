047de46522125cc15cb9d553477225107297c95e
Bassem S.Mosaad <bassemsaeed620@gmail.com>
2023-08-11 12:12:41 +0300

    Box -> global & Experience UI

--- src/pages/Experiences.jsx
+++ src/pages/Experiences.jsx
@@ -1,7 +1,36 @@
+import { Container, Row, Col } from "react-bootstrap"
+import { Btn } from "../components"
+import OpenInNewOutlinedIcon from '@mui/icons-material/OpenInNewOutlined';
+
 const Experiences = () => {
     return (
-        <div className="section" id="experience">
-            Experiences
+        <div className="boxesSection section" id="experience">
+            <Container>
+                <div className="textBox mx-auto mb-5">
+                    <span>🧑‍💻 Experiences</span>
+                </div>
+                <Row className="m-0 p-0">
+                    <Col sm={12} md={6} lg={4} className="boxesContainer">
+                        <div className="box experience">
+                            <h5 className="title">Backend Hackathon</h5>
+                            <p>
+                                During this week at Orange Digital Center, I worked on developing a RESTful API backend
+                                for Orange Company. The objective of the project was to create a platform for monitoring
+                                the courses offered by Orange's partners, such as Instant and Amit.
+                            </p>
+                            <div className="timeBound">
+                                <span className="duration">1 Week</span>
+                                <div>
+                                    <span className="date">16/09/2022</span>
+                                    <span>&nbsp;-&nbsp;</span>
+                                    <span className="date">22/09/2022</span>
+                                </div>
+                            </div>
+                            <Btn text="More" toLink="" logo={<OpenInNewOutlinedIcon />}/>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -7,19 +7,19 @@ import OpenInNewOutlinedIcon from '@mui/icons-material/OpenInNewOutlined';
 
 const Projects = () => {
     return (
-        <div className="projects section" id="projects">
+        <div className="boxesSection section" id="projects">
             <div className="textBox mx-auto mb-5">
                 <span>🧑‍💻 Projects</span>
             </div>
             <Container>
                 <Row className="m-0 p-0">
-                    <Col sm={12} md={6} lg={4} xl={3} className="projectContainer">
+                    <Col sm={12} md={6} lg={4} xl={3} className="boxesContainer">
                         <div className="box">
                             <img src={web} alt="" className="img-fluid"/>
                             <h5 className="title">E-Commerce NodeJS Backend Server</h5>
                             <p>
                                 The project also iertrtertetertertertertencorporates additional features, including security measures 
-                                such as request size limits, rate limiting, anti-CSRF tokens, prevention of asdasdadsasdasdaasdasdasdasdasdsdasdasd
+                                such as request size limits, rate limiting, anti-CSRF tsdlkamdklmskldm masdlmasldkakl lamsdlkadsm okens, prevention of asdasdadsasdasdaasdasdasdasdasdsdasdasd
                             </p>
                             <div className="btns d-flex align-items-center">
                                 <Btn toLink={null} text="More" logo={<OpenInNewOutlinedIcon />} classes="w-50 me-1"/>
