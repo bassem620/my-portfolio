0fbf071abdc64a2bfc2fabf9714ff9af5f0edb82
Bassem S.Mosaad <bassemsaeed620@gmail.com>
2023-08-14 22:42:30 +0300

    edits for hosting on render

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <>
         <Nav />
         <Routes>
-            <Route path="/my-portfolio" element={<PagesContainer />}/>
+            <Route index element={<PagesContainer />}/>
         </Routes>
         <Footer />
         </>
