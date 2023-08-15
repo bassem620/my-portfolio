9d4eb615d789f80dfcface0fef49c6ad8adef6f6
Bassem S.Mosaad <bassemsaeed620@gmail.com>
2023-08-15 03:00:09 +0300

    ProjectBox

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,13 @@
 import Nav from './Nav';
 import Footer from './Footer';
 import Btn from './Btn';
+import ProjectBox from './ProjectBox';
 import WhatsappBtn from './WhatsappBtn';
 
 export {
     Nav,
     Footer,
     Btn,
+    ProjectBox,
     WhatsappBtn,
 }
